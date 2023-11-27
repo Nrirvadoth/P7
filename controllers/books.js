7aa8bfbb8793cf752028af6ae5c664139aa154b7
Mat <mathurin.malandain+github@gmail.com>
2023-11-27 11:38:23 +0100

    Creation routes Book

---
+++ controllers/books.js
@@ -0,0 +1,15 @@
+const Book = require('../models/Book')
+
+exports.getAllBooks = (req, res, next) => {}
+
+exports.getOneBooks = (req, res, next) => {}
+
+exports.bestRating = (req, res, next) => {}
+
+exports.createBook = (req, res, next) => {}
+
+exports.modifyBook = (req, res, next) => {}
+
+exports.deleteBook = (req, res, next) => {}
+
+exports.addRating = (req, res, next) => {}
