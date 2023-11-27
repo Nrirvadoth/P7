309d096a52f1cf627e1aed3304104d62c98720cc
Mat <mathurin.malandain+github@gmail.com>
2023-11-27 11:25:45 +0100

    créations models data

---
+++ models/Book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const bookSchema = mongoose.Schema({
+    userId : { type: String, required: true},
+    title : { type: String, required: true},
+    author : { type: String, required: true},
+    imageUrl : { type: String },
+    year: { type: Number },
+    genre: { type: String },
+    ratings : [
+        {
+        userId : { type: String },
+        grade : { type: Number }
+        }
+    ],
+    averageRating : { type: Number}
+})
+
+module.exports = mongoose.model('Book', bookSchema)
