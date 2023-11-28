0d5fcdcac5f9f42533dd6d325bfd6eedd30333de
Mat <mathurin.malandain+github@gmail.com>
2023-11-28 19:39:07 +0100

    correction middleware auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.header.authorization.split(' ')[1]
+    const token = req.headers.authorization.split(' ')[1]
     const tokenDecoded = jwt.verify(token, 'OCP6-token_1548re58e')
     const userId = tokenDecoded.userId
     req.auth = { userId: userId }
