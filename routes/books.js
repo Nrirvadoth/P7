1013b0c44e33f6c885cbd5e15e1e05d4b08ed7e6
Mat <mathurin.malandain+github@gmail.com>
2023-12-06 16:00:48 +0100

    bestrating fonctionnel (correction routing)

--- routes/books.js
+++ routes/books.js
@@ -5,8 +5,8 @@ const auth = require('../middlewares/auth')
 const multer = require('../middlewares/multer-config')
 
 router.get('/', bookCtrl.getAllBooks)
-router.get('/:id', bookCtrl.getOneBook)
 router.get('/bestrating', bookCtrl.bestRating)
+router.get('/:id', bookCtrl.getOneBook)
 
 router.post('/', auth, multer, bookCtrl.createBook)
 router.put('/:id', auth, multer, bookCtrl.modifyBook)
