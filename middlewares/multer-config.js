34110c3e2fd28f2bb33634e065c4c6454e9b817f
Mat <mathurin.malandain+github@gmail.com>
2023-12-13 12:21:12 +0100

    ajout sharp

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,6 +1,7 @@
 const multer = require('multer')
+const sharp = require('sharp')
 
-const MIME_TYPES = {
+/* const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png',
@@ -18,4 +19,23 @@ const storage = multer.diskStorage({
   },
 })
 
-module.exports = multer({ storage }).single('image')
+module.exports = multer({ storage }).single('image') */
+
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+module.exports = (upload.single('image'), async (req, res, next) => {
+  console.log(req.file)
+  const { originalname, buffer } = req.file
+  let name = originalname.split(' ').join('_')
+  name = name.split('.')[0]
+  name += Date.now() + '.webp'
+
+  await sharp(buffer)
+    .resize(420, 540)
+    .webp()
+    .toFile('./images' + name)
+  
+  next()
+})
