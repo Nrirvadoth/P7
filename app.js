4d3e0fa3115bbb9aa20de94721662ec94caa4134
Mat <mathurin.malandain+github@gmail.com>
2024-01-12 16:34:17 +0100

    utilisation dotenv connexion BDD mongoDB

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
 const userRoutes = require('./routes/users')
 const bookRoutes = require('./routes/books')
-const app = express();
+const app = express()
 const path = require('path')
 
-mongoose.connect('mongodb+srv://mathurinmalandain:764G8rRa82HIPLpj@cluster0.6vvwj8u.mongodb.net/?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true})
-.then(console.log("mongodb connected successfully...."))
-.catch(err =>console.log(err));
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+  )
+  .then(console.log('mongodb connected successfully....'))
+  .catch((err) => console.log(err))
 
-app.use(express.json());
+app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  )
+  next()
+})
 
-app.use('/api/books', bookRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/books', bookRoutes)
+app.use('/api/auth', userRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
-module.exports = app;
+module.exports = app
